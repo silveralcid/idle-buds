b840245f6f421ad4c4a99197d028e72432aaf214
silveralcid <silveralcid@gmail.com>
2024-11-15 01:49:20 -0500

    feat: added placeholder text to sidebar

--- idle-buds/src/components/Sidebar.tsx
+++ idle-buds/src/components/Sidebar.tsx
@@ -1,11 +1,44 @@
 export const Sidebar = () => {
     return (
-      <ul>
-        <li><a>Skills</a></li>
-        <li><a>Inventory</a></li>
-        <li><a>Combat</a></li>
-        <li><a>Shop</a></li>
-        <li><a>Settings</a></li>
-      </ul>
+      <div className="flex flex-col gap-6">
+        <div>
+          <h2 className="font-bold text-lg mb-2">Actions</h2>
+          <ul className="menu menu-lg bg-base-100 rounded-lg">
+            <li><a>Shop</a></li>
+            <li><a>Bank</a></li>
+            <li><a>Buds</a></li>            
+          </ul>
+        </div>
+  
+        <div>
+          <h2 className="font-bold text-lg mb-2">Combat</h2>
+          <ul className="menu menu-lg bg-base-100 rounded-lg">
+            <li><a>Level</a></li>
+            <li><a>Attack</a></li>
+            <li><a>Defense</a></li>
+            <li><a>Health</a></li>            
+            <li><a>Efficiency</a></li>
+          </ul>
+        </div>
+        
+        <div>
+          <h2 className="font-bold text-lg mb-2">Passive</h2>
+          <ul className="menu menu-lg bg-base-100 rounded-lg">
+            <li><a>Farming</a></li>
+          </ul>
+        </div>
+        
+        <div>
+          <h2 className="font-bold text-lg mb-2">Non-Combat</h2>
+          <ul className="menu menu-lg bg-base-100 rounded-lg">
+            <li><a>Woodcutting</a></li>
+            <li><a>Fishing</a></li>
+            <li><a>Cooking</a></li>
+            <li><a>Mining</a></li>
+            <li><a>Smithing</a></li>
+            <li><a>Crafting</a></li>
+          </ul>
+        </div>
+      </div>
     );
   };
