696500394639a36694056b4ff6ae7555e0a4fe2e
silveralcid <silveralcid@gmail.com>
2024-11-19 10:28:02 -0500

    feat: add item enums

--- idle-buds/src/enums/item.enums.ts
+++ idle-buds/src/enums/item.enums.ts
@@ -1,22 +1,60 @@
-  
-  export enum EquipmentType {
-    WEAPON = 'WEAPON',
-    ARMOR = 'ARMOR',
-    SHIELD = 'SHIELD',
-    TOOL = 'TOOL',
-    ACCESSORY = 'ACCESSORY'
-  }
-  
-  export enum ToolType {
-    AXE = 'AXE',
-    PICKAXE = 'PICKAXE',
-    FISHING_ROD = 'FISHING_ROD',
-  }
-  
-  export enum EquipmentQuality {
-    NORMAL = 'NORMAL',
-    FINE = 'FINE',
-    SUPERIOR = 'SUPERIOR',
-    EXCEPTIONAL = 'EXCEPTIONAL',
-    PERFECT = 'PERFECT'
-  }
+// src/enums/item.enums.ts
+export enum ItemType {
+  EQUIPMENT = 'EQUIPMENT',
+  RESOURCE = 'RESOURCE',
+  CONSUMABLE = 'CONSUMABLE',
+  MATERIAL = 'MATERIAL',
+  SPECIAL = 'SPECIAL'
+}
+
+export enum EquipmentType {
+  WEAPON = 'WEAPON',
+  ARMOR = 'ARMOR',
+  SHIELD = 'SHIELD',
+  TOOL = 'TOOL',
+  ACCESSORY = 'ACCESSORY'
+}
+
+export enum WeaponType {
+  MELEE = 'MELEE',
+  RANGED = 'RANGED',
+  MAGIC_STAFF = 'MAGIC_STAFF'
+}
+
+export enum ToolType {
+  AXE = 'AXE',
+  PICKAXE = 'PICKAXE',
+  FISHING_ROD = 'FISHING_ROD',
+  WATERING_CAN = 'WATERING_CAN',
+  SMITHING_HAMMER = 'SMITHING_HAMMER'
+}
+
+export enum MaterialType {
+  CRAFTING = 'CRAFTING',
+  SMITHING = 'SMITHING',
+  COOKING = 'COOKING',
+  SPECIAL = 'SPECIAL'
+}
+
+export enum ConsumableType {
+  FOOD = 'FOOD',
+  POTION = 'POTION',
+  BAIT = 'BAIT'
+}
+
+export enum EquipmentQuality {
+  NORMAL = 'NORMAL',
+  FINE = 'FINE',
+  SUPERIOR = 'SUPERIOR',
+  EXCEPTIONAL = 'EXCEPTIONAL',
+  PERFECT = 'PERFECT'
+}
+
+export enum EquipmentSlot {
+  MAIN_HAND = 'MAIN_HAND',
+  OFF_HAND = 'OFF_HAND',
+  BODY = 'BODY',
+  ACCESSORY_1 = 'ACCESSORY_1',
+  ACCESSORY_2 = 'ACCESSORY_2',
+  TOOL = 'TOOL'
+}
