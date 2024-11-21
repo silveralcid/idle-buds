28a4d450eae9dc55aa6b4b1fd21f32d1e40942e9
Silver Alcid <silveralcid@gmail.com>
2024-11-21 18:08:18 -0500

    feat: add ore data

--- idle-buds/src/data/allResources.data.ts
+++ idle-buds/src/data/allResources.data.ts
@@ -1,8 +1,11 @@
 import { Resource } from '../types/resource.types';
 import { woodResources } from './resources/wood.data';
+import { oreResources } from './resources/ore.data';
+
 // Import other resource types as needed
 
 export const allResources: Resource[] = [
-  ...woodResources
+  ...woodResources,
+  ...oreResources,
   // Add other resource arrays here
 ];
