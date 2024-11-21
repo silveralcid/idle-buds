ef25731f2ca7bab13c9f3a522a27cef89325d1cf
Silver Alcid <silveralcid@gmail.com>
2024-11-20 20:47:16 -0500

    feat: add more buds for testing

--- idle-buds/src/data/buds/budSpecies.data.ts
+++ idle-buds/src/data/buds/budSpecies.data.ts
@@ -3,11 +3,57 @@ import { budBase } from '../../types/budBase.types';
 export const budSpecies: budBase[] = [
   {
     speciesId: '001',
-    name: 'Woodland Bud',
+    name: 'Poodoo',
     description: 'A Bud that thrives in forested areas, adept at lumbering.',
     spriteRef: '/assets/sprites/buds/slice-2.png',
     allowedTasks: ['lumbering', 'mining'],
     primaryAffinity: 'grass',
+    // Uncomment and fill in if needed
+    // secondaryAffinity: 'ground',
+    // baseStats: {
+    //   health: 50,
+    //   wisdom: 30,
+    //   attack: 40,
+    //   defense: 35,
+    //   dexterity: 25,
+    // },
+    // moves: {
+    //   normal: ['leaf_slash', 'bark_bash'],
+    //   special: 'forest_fury',
+    // },
+    // primaryPassive: 'photosynthesis',
+    // secondaryPassive: 'rooted',
+  },
+  {
+    speciesId: '002',
+    name: 'Wach',
+    description: 'A Bud that thrives in forested areas, adept at lumbering.',
+    spriteRef: '/assets/sprites/buds/slice-3.png',
+    allowedTasks: ['lumbering', 'mining'],
+    primaryAffinity: 'water',
+    // Uncomment and fill in if needed
+    // secondaryAffinity: 'ground',
+    // baseStats: {
+    //   health: 50,
+    //   wisdom: 30,
+    //   attack: 40,
+    //   defense: 35,
+    //   dexterity: 25,
+    // },
+    // moves: {
+    //   normal: ['leaf_slash', 'bark_bash'],
+    //   special: 'forest_fury',
+    // },
+    // primaryPassive: 'photosynthesis',
+    // secondaryPassive: 'rooted',
+  },
+  {
+    speciesId: '003',
+    name: 'Fothy',
+    description: 'A Bud that thrives in forested areas, adept at lumbering.',
+    spriteRef: '/assets/sprites/buds/slice-1.png',
+    allowedTasks: ['lumbering', 'mining'],
+    primaryAffinity: 'fire',
     // Uncomment and fill in if needed
     // secondaryAffinity: 'ground',
     // baseStats: {
