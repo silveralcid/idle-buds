dc83f2368f9069ee7c3a7c6331b19dc19d307011
Silver Alcid <silveralcid@gmail.com>
2024-11-23 20:01:00 -0500

    chore: game loop clean

--- src/hooks/useGameLoop.ts
+++ src/hooks/useGameLoop.ts
@@ -1,16 +1,13 @@
 import { useEffect } from 'react';
 import { useGameStore } from '../stores/game.store';
 import { useHunterStore } from '../stores/hunter.store';
-import { useActiveBudStore } from '../stores/active-bud.store';
 import { GameConfig } from '../constants/game-config';
 import { processGathering } from '../utils/gathering.utils';
 
 export const useGameLoop = () => {
   const isPaused = useGameStore((state) => state.isPaused);
   const updateHunterActivityProgress = useHunterStore((state) => state.updateHunterActivityProgress);
-  const updateBudProgress = useActiveBudStore((state) => state.updateBudProgress);
   const hunterActivity = useHunterStore((state) => state.currentActivity);
-  const budActivities = useActiveBudStore((state) => state.budActivities);
 
   useEffect(() => {
     let lastTime = performance.now();
@@ -34,13 +31,9 @@ export const useGameLoop = () => {
         if (hunterActivity) {
           updateHunterActivityProgress(tickDeltaTime);
         }
-        
-        if (Object.keys(budActivities).length > 0) {
-          updateBudProgress(tickDeltaTime);
-        }
 
         // Process gathering for both hunter and buds if there are active gatherers
-        if (hunterActivity || Object.keys(budActivities).length > 0) {
+        if (hunterActivity) {
           processGathering(tickDeltaTime);
         }
 
@@ -59,8 +52,6 @@ export const useGameLoop = () => {
   }, [
     isPaused, 
     updateHunterActivityProgress, 
-    updateBudProgress, 
     hunterActivity, 
-    budActivities
   ]);
 };
